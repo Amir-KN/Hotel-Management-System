54bc55337dd6d32b1b713e364ced5e2f0759317d
Amirhossein Kahrobaeian <amirhoseinn1412@gmail.com>
2023-03-11 01:03:55 +0330

    Update server and client file and use class, add basic feature about signup and signin, add basic json file

--- Client.cpp
+++ Client.cpp
@@ -7,37 +7,54 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include "includes/json.hpp"
 #include <iostream>
+#include <sstream>
 #include <fstream>
-#include <vector>
 #define MAX_LEN_MESS 128
 
 using namespace std;
+using json = nlohmann::json;
+
+vector<string> BreakString(string str)
+{
+    stringstream ss(str);
+    string word;
+    vector<string> words;
+
+    while (ss >> word)
+        words.push_back(word);
+    return words;
+}
 
 class Client
 {
 public:
     Client()
     {
-        ip = "127.0.0.1";
-        port = 8000;
+        ifstream C("includes/config.json"), E("includes/Errors.json");
+        json Config;
+
+        Config = json::parse(C);
+        Errors = json::parse(E);
+
+        ip = Config["hostName"].get<string>();
+        port = Config["commandChannelPort"].get<int>();
     }
 
     void Run()
     {
         ConnectToServer();
         string command;
-        bool exit_or_not;
+        bool is_sign_ok;
         while (true)
         {
-            cout << "Write Command :\n  --> signin <username> <password>\n  --> signup <username>\n  --> exit\n"
-                 << endl;
-            cin >> command ;
+            cout << "Write Command :\n  --> signin <username> <password>\n  --> signup <username>\n  --> exit\n" << endl;
+            command = GetFirstCommand();
             send(server_fd, command.c_str(), strlen(command.c_str()), 0);
             string recv_mess = Recv(server_fd);
-            cout << recv_mess << endl;
-            exit_or_not = ExitorNot(recv_mess);
-            if (exit_or_not)
+            is_sign_ok = CheckSign(recv_mess, server_fd);
+            if (!is_sign_ok)
                 break;
         }
     }
@@ -46,6 +63,13 @@ private:
     int server_fd;
     int port;
     string ip;
+    json Errors;
+
+    void PrintError(string error_number)
+    {
+        string fail = "Failed to read Error number " + error_number;
+        cout << Errors.value(error_number, fail) << endl;
+    }
 
     void ConnectToServer()
     {
@@ -55,15 +79,80 @@ private:
 
         server_address.sin_family = AF_INET;
         server_address.sin_port = htons(port);
-        server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+        server_address.sin_addr.s_addr = inet_addr(ip.c_str());
 
         if (connect(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
         { // checking for errors
-            cout << "Fail to connect server !" << endl;
+            PrintError("404");
         }
     }
 
-    
+    string GetUserInfo()
+    {
+        string command = "", word;
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> word;
+            command += word + " ";
+        }
+        return command;
+    }
+
+    bool CheckSign(string recv_mess_from_ser, int server_fd)
+    {
+        string error_number;
+
+        if (recv_mess_from_ser == "SIGNUP_OK")
+        {
+            PrintError("311");
+            string user_info = GetUserInfo();
+            send(server_fd, user_info.c_str(), sizeof(user_info.c_str()), 0);
+            error_number = Recv(server_fd);
+            PrintError(string(error_number));
+        }
+        else if (recv_mess_from_ser == "SIGNUP_NOT_OK")
+        {
+            PrintError("451");
+        }
+
+        else if (recv_mess_from_ser == "SIGNIN_OK")
+        {
+            PrintError("230");
+            Menu();
+        }
+        else if (recv_mess_from_ser == "SIGNIN_NOt_OK")
+            PrintError("430");
+
+        else if (recv_mess_from_ser == "EXIT_OK")
+            return false;
+
+        else
+        {
+            // Problem in CheckSign func.
+            cout << "Problem in CheckSign func." << endl;
+        }
+
+        return true;
+    }
+
+    string GetFirstCommand()
+    {
+        string command = "";
+        vector<string> commands;
+        while (true)
+        {
+            getline(cin, command);
+            commands = BreakString(command);
+            if ((command == "") || (command == " ") || (command == "\n") || (command == "  "))
+                continue;
+            if (commands.size() == 0 || (!((commands[0] == "signup") || (commands[0] == "signin") || (commands[0] == "exit"))))
+            {
+                cout << "Invalid Command" << endl;
+                continue;
+            }
+            return command;
+        }
+    }
 
     string Recv(int server_fd)
     {
@@ -73,14 +162,9 @@ private:
         return string(buffer);
     }
 
-    bool ExitorNot(string mess){
-        if (mess == "exit")
-            return true;
-        else   
-            return false;
+    void Menu()
+    {
     }
-
-
 };
 
 int main(int argc, char const *argv[])
--- Server.cpp
+++ Server.cpp
@@ -10,113 +10,205 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
+#include "includes/json.hpp"
 
 using namespace std;
-#define MAX_LEN_MESS 128
+using json = nlohmann::json;
 
-int SetupServer(int port)
+#define MAX_LEN_MESS 512
+
+
+vector<string> BreakString(string str)
 {
-    struct sockaddr_in address;
-    int server_fd;
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    stringstream ss(str);
+    string word;
+    vector<string> words;
 
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-
-    listen(server_fd, 4);
-
-    return server_fd;
+    while (ss >> word)
+        words.push_back(word);
+    return words;
 }
 
-int AcceptClient(int server_fd)
+class Server
 {
-    int client_fd;
-    struct sockaddr_in client_address;
-    int address_len = sizeof(client_address);
-    client_fd = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t *)&address_len);
+public:
+   Server(){
+        ifstream C("includes/config.json"), E("includes/Errors.json");
+        json Config;
 
-    return client_fd;
-}
+        Config = json::parse(C);
+        Errors = json::parse(E);
 
+        port = Config["commandChannelPort"].get<int>();
+    }
 
-string Recv(int client_fd)
-{
-    char buffer[MAX_LEN_MESS] = {0};
-    memset(buffer, 0, MAX_LEN_MESS);
-    recv(client_fd, buffer, MAX_LEN_MESS, 0);
-    return string(buffer);
-}
-
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, max_sd;
-    char buffer[1024] = {0};
-    fd_set master_set, working_set;
-
-    server_fd = SetupServer(8000);
-
-    FD_ZERO(&master_set);
-    max_sd = server_fd;
-    FD_SET(server_fd, &master_set);
-    FD_SET(fileno(stdin), &master_set);
-
-    write(1, "*** Welcome to Hotel Management System ***\nPlese set time\n", 59);
-
-    while (true)
+    void Run()
     {
-        working_set = master_set;
-        select(max_sd + 1, &working_set, NULL, NULL, NULL);
+        SetupServer();
+        char buffer[1024] = {0};
 
-        for (int i = 0; i <= max_sd; i++)
+        FD_ZERO(&master_set);
+        max_sd = server_fd;
+        FD_SET(server_fd, &master_set);
+        FD_SET(fileno(stdin), &master_set);
+
+        write(1, "*** Welcome to Hotel Management System ***\nPlese set time\n", 59);
+
+        while (true)
         {
-            if (FD_ISSET(i, &working_set))
+            working_set = master_set;
+            select(max_sd + 1, &working_set, NULL, NULL, NULL);
+
+            for (int i = 0; i <= max_sd; i++)
             {
-                if (i == 0)
-                { // get input from user with command
-                    memset(buffer, 0, 1024);
-                    read(0, buffer, 1024);
-                    // handle set time
-                }
-
-                else if (i == server_fd)
-                { // new clinet
-                    new_socket = AcceptClient(server_fd);
-                    FD_SET(new_socket, &master_set);
-                    if (new_socket > max_sd)
-                        max_sd = new_socket;
-                    cout << "New Client Connected. fd = " << new_socket << endl;
-                }
-
-                else
-                { // recv message from client
-                    int bytes_received;
-                    memset(buffer, 0, 1024);
-                    bytes_received = recv(i, buffer, 1024, 0);
-
-                    if (bytes_received == 0)
-                    { // EOF
-                        printf("client fd = %d closed\n", i);
-                        close(i);
-                        FD_CLR(i, &master_set);
-                        continue;
+                if (FD_ISSET(i, &working_set))
+                {
+                    if (i == 0)
+                    { // get input from user with command
+                        GetFromBuffer();
                     }
 
-                    printf("client %d: %s\n", i, buffer);
-                    if (string(buffer) == "exit")
-                        send(i, "exit", 5, 0);
+                    else if (i == server_fd)
+                    { // new clinet
+                        new_socket = AcceptClient(server_fd);
+                        FD_SET(new_socket, &master_set);
+                        if (new_socket > max_sd)
+                            max_sd = new_socket;
+                        cout << "New Client Connected. fd = " << new_socket << endl;
+                    }
+
                     else
-                        send(i, "Your message is received!", 26, 0);
+                    { // recv message from client
+                        GiveMessFromClient(i);
+                    }
                 }
             }
         }
     }
+
+private:
+    int server_fd, new_socket, max_sd, port;
+    char buffer[MAX_LEN_MESS] = {0};
+    fd_set master_set, working_set;
+    json Errors;
+
+    void SetupServer()
+    {
+        struct sockaddr_in address;
+        server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+        int opt = 1;
+        setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(port);
+
+        bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+        listen(server_fd, 4);
+    }
+
+    int AcceptClient(int server_fd)
+    {
+        int client_fd;
+        struct sockaddr_in client_address;
+        int address_len = sizeof(client_address);
+        client_fd = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t *)&address_len);
+        return client_fd;
+    }
+
+
+    void GetFromBuffer()
+    {
+        
+        memset(buffer, 0, MAX_LEN_MESS);
+        read(0, buffer, MAX_LEN_MESS);
+        // handle set time
+    }
+
+    void GiveMessFromClient(int client_fd)
+    {
+        int bytes_received;
+        memset(buffer, 0, MAX_LEN_MESS);
+        bytes_received = recv(client_fd, buffer, 1024, 0);
+
+        if (bytes_received == 0)
+        { // EOF
+            printf("client fd = %d closed\n", client_fd);
+            close(client_fd);
+            FD_CLR(client_fd, &master_set);
+            return;
+        }
+
+        printf("client %d: %s\n", client_fd, buffer);
+        CommandHandler(string(buffer), client_fd);
+    }
+
+    string Recv(int client_fd)
+    {
+        memset(buffer, 0, MAX_LEN_MESS);
+        recv(client_fd, buffer, MAX_LEN_MESS, 0);
+        return string(buffer);
+    }
+
+    void CommandHandler(string command_line, int client_fd)
+    {
+        vector<string> command = BreakString(command_line);
+
+        if (command[0] == "signup")
+        {
+            /*
+            if (username exist)
+                send("SIGNUP_NOT_OK")
+
+            if (username not exist) {
+                send("SIGNUP_OK");
+                recv()
+                bool is_info_ok = check_recv()
+                if (recv is ok)
+                    send("231")
+                else
+                    send("503")
+
+
+                save info of new use in json;
+            }
+
+            */
+            // delete 3 line below
+            send(client_fd, "SIGNUP_OK", 10, 0);
+            Recv(client_fd);
+            send(client_fd, "231", 10, 0);
+        }
+
+        else if (command[0] == "signin")
+        {
+            /*
+            if (ok) :
+                send("SIGNIN_OK")
+
+            send(client_fd, "--> signin command is recved from client <--", 45, 0);
+            */
+
+            // delete line below
+            send(client_fd, "SIGNIN_OK", 10, 0);
+        }
+
+        else if (command[0] == "exit")
+        {
+            send(client_fd, "EXIT_OK", 8, 0);
+        }
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Server Server;
+    Server.Run();
 
     return 0;
 }
